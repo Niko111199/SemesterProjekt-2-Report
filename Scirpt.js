2fcf45378992b60471a679aae6338f0b2d01775c
Niko111199 <niko111199@hotmail.com>
2025-03-12 10:18:54 +0100

    tidsplan + problemformulering + javascript

---
+++ Scirpt.js
@@ -0,0 +1,7 @@
+       function updateCharCount() {
+            const content = document.getElementById("content").innerText;
+            document.getElementById("charCounter").innerText = "[Antal Tegn: " + content.length + "]";
+        }
+        
+        document.addEventListener("input", updateCharCount);
+        updateCharCount();
